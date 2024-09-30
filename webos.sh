0d216df599229066b069b154403834b5d7387453
AshMacPro <ashot0907@gmail.com>
2024-09-30 15:31:20 +0400

    added starting script

---
+++ webos.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Run all commands with superuser privileges
+sudo bash -c '
+# Navigate to the backend directory
+cd backend
+
+# Start the Node server
+node server.js &
+
+# Start the Express server
+node express.js &
+
+# Navigate back to the parent directory
+cd ..
+
+# Start the application on PORT 222
+PORT=222 npm start
+'
